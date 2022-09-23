828f00ea5ef5ef8d2925cac04d3afc7a348e3720
lucasceolin <79987087+lucasceolin@users.noreply.github.com>
2022-09-22 22:11:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ cotacao = json.loads(requisicao.text)
 dolar = cotacao['results']['currencies']['USD']['buy']
 euro = cotacao['results']['currencies']['EUR']['buy']
 real = float(input('Informe um valor desejado em R$: '))
-dolarConvertido = real*dolar
-euroConvertido = real*euro
+dolarConvertido = real/dolar
+euroConvertido = real/euro
 print(f'Valor em dolar: {dolarConvertido}')
 print(f'Valor em euro: {euroConvertido}')
